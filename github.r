7823d10e1c43ddee04fce43c7c0a8dcbf6268106
mathbeveridge <abeverid@macalester.edu>
2022-01-24 13:14:33 -0600

    updating with unnamed figures

---
+++ github.r
@@ -0,0 +1,47 @@
+#### 1. Sign up at GitHub.com ################################################
+
+## If you do not have a GitHub account, sign up here:
+## https://github.com/join
+
+# ----------------------------------------------------------------------------
+
+#### 2. Install git ##########################################################
+
+## If you do not have git installed, please do so: 
+## Windows ->  https://git-scm.com/download/win
+## Mac     ->  https://git-scm.com/download/mac
+## Linux   ->  https://git-scm.com/download/linux
+##             or: $ sudo dnf install git-all
+
+# ----------------------------------------------------------------------------
+
+### 3. Configure git with Rstudio ############################################
+
+## set your user name and email:
+usethis::use_git_config(user.name = "mathbeveridge", user.email = "abeverid@macalester.edu")
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## set personal access token:
+credentials::set_github_pat("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")
+
+## or store it manually in '.Renviron':
+usethis::edit_r_environ()`
+## store your personal access token with: GITHUB_PAT=xxxyyyzzz
+## and make sure '.Renviron' ends with a newline
+
+# ----------------------------------------------------------------------------
+
+#### 4. Verify settings ######################################################
+
+usethis::git_sitrep()
+
+## Your username and email should be stated correctly in the output. 
+## Also, the report shoud cotain something like:
+## 'Personal access token: '<found in env var>''
+
+# ----------------------------------------------------------------------------
+
+## THAT'S IT!
